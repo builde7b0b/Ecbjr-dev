fc793c44515a972951bd361085d8372d25f89623
builde7b0b <ggives3@gmail.com>
2023-01-07 20:58:01 -0800

    Moved Blog Post into Blog Folder, Updated Navigation and Changed Links to include Absolute Paths

--- js/main.js
+++ js/main.js
@@ -41,3 +41,32 @@
     });
 })(jQuery);
 
+
+
+// START BLOG PAGE FUNCTIONALITY 
+//Inspiration: https://blog.maximeheckel.com/posts/the-magical-world-of-particles-with-react-three-fiber-and-shaders/
+//Wiki: https://en.wikipedia.org/wiki/Andrei_Tarkovsky
+
+//A polyfill for ScrollTimeline:
+import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
+
+//ScrollTracker Animation:
+var scrollTracker = document.getElementById('scroll-tracker');
+
+var scrollTrackingTimeline = new ScrollTimeline({
+  source: document.scrollingElement,
+  orientation: 'block',
+  scrollOffsets: [CSS.percent(0), CSS.percent(100)],
+});
+
+scrollTracker.animate(
+  {
+    transform: ['scaleX(0)', 'scaleX(1)']
+  },
+  {
+    timeline: scrollTrackingTimeline,
+  }
+)
+
+
+
